743d881cd5aa9320854e6191d50881cea53c267c
Smoonsiwoo <maposam3@gmail.com>
2024-08-28 15:49:26 +0900

    Add files via upload

--- pycoding01/learning01.py
+++ pycoding01/learning01.py
@@ -1,3 +1,14 @@
-num1 = 10
-num2= 20
-print (num1+num2)
+# num1 = int (input("첫번째 숫자 입력:"))
+# num2 = int (input("두번째 숫자 입력:"))
+
+
+# Answer = num1+ num2
+
+# print("두 수의 합: ", Answer)
+
+#생년월일 입력받기
+
+BirthDate = int(input("생년월일을 입력하세요:"))
+
+#계산후 출력하기
+print ("당신의 나이는",2025 - BirthDate, "살입니다.")
