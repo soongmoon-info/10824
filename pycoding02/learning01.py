3048c982ca6010d4084ae2b2c4fd349e28131416
Smoonsiwoo <maposam3@gmail.com>
2024-09-06 13:48:26 +0900

    Add files via upload

---
+++ pycoding02/learning01.py
@@ -0,0 +1,19 @@
+# #조건문
+
+# x = 5
+# if x >= 10 :
+#   print ("조건이 T/F일 때 출력")
+# print("이건 언제 출력?")
+
+#나이 입력받기
+Age = int(input ("나이를 입력하세요:"))
+
+#판단 후 출력"
+
+if Age <= 13 :
+  print ("어린이")
+elif 13 < Age <= 19 :
+  print ("청소년")
+elif Age>19:
+  print ("딸피")
+  
