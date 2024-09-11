edcc77f3661cfe2d23e6b0af03b938cc7bce6a25
Smoonsiwoo <maposam3@gmail.com>
2024-09-11 15:45:52 +0900

    Add files via upload

--- pycoding02/learning01.py
+++ pycoding02/learning01.py
@@ -1,19 +1,43 @@
-# #조건문
+# # #조건문
 
-# x = 5
-# if x >= 10 :
-#   print ("조건이 T/F일 때 출력")
-# print("이건 언제 출력?")
+# # x = 5
+# # if x >= 10 :
+# #   print ("조건이 T/F일 때 출력")
+# # print("이건 언제 출력?")
 
-#나이 입력받기
-Age = int(input ("나이를 입력하세요:"))
+# #나이 입력받기
+# Age = int(input ("나이를 입력하세요:"))
 
-#판단 후 출력"
+# #판단 후 출력"
 
-if Age <= 13 :
-  print ("어린이")
-elif 13 < Age <= 19 :
-  print ("청소년")
-elif Age>19:
-  print ("딸피")
+# if Age <= 13 :
+#   print ("어린이")
+# elif 13 < Age <= 19 :
+#   print ("청소년")
+# elif Age>19:
+#   print ("딸피")
+
+
+# battery = int(input("베터리를 입력하세요"))
+
+# if battery < 2 :
+#   print ("종료")
+# elif battery <= 10 :
+#   print ("충전을 해주세요")
+# elif battery <= 20 :
+#   print ("저전력 모드")
+# elif battery <= 100 :
+#   print ("정상작동중")
+
+# tier = input("티어 입력: ")
+
+# Champ = input ("챔피언을 입력하세요: ")
+
+# if tier == "브론즈" and Champ == "야스오":
   
+#   print ("과학자")
+
+#포함연산자
+call = input ("-없이 전화번호 입력: ")
+if "-" in call :
+  print ("-를 제외하고 입력해주세요")
