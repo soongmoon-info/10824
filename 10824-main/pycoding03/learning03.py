f5b944d86349510f8410283ea20d4daecdfb752d
Smoonsiwoo <maposam3@gmail.com>
2024-10-02 15:52:02 +0900

    Add files via upload

--- 10824-main/pycoding03/learning03.py
+++ 10824-main/pycoding03/learning03.py
@@ -1,6 +1,38 @@
-i = 0
+# i = 0
 
-while i < 3 :
-  print(i)
-  i = i+1
-print ("이거 끝나면 정리하고 가자")
+# while i < 3 :
+#   print(i)
+#   i = i+1
+# print ("이거 끝나면 정리하고 가자")
+
+
+# for i in range (0,3,1) :
+#   print (i)
+  
+  
+# for i in range (0,3) :
+#   print (i)
+  
+# for i in range (3) :
+#   print (i)
+
+
+
+# print (list(range(0,3,1)))
+
+# a = ['사과', '배', '귤', '샤인머', '망고']
+# for i in a: 
+#   print (i)
+  
+#   if i == '귤':
+#     print ("귤 찾음")
+#     continue
+#     print (i)
+
+
+a = ['사과', '배', '귤', '샤인머', '망고']
+for index, value in enumerate(a):
+  if value == '배':
+    a [index] = 0
+  
+print (a)
---
+++ 10824-main/pycoding04/learning01.py
@@ -0,0 +1,15 @@
+# i = 2
+# while i < 10:
+#   print(i, '단')
+#   j = 1
+#   while j < 10 :
+#     print(i,'*',j,'=',i * j)
+#     j = j+1
+#   i = i+1
+  
+  
+  
+  
+a = [[1,2],[3,4],[5,6]]
+for i in a:
+  print (i)
